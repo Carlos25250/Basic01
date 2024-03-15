05c51aa65e185856908c83a5bff4ada0e44fc90e
Carlos25250 <2410558@cesba-queretaro.edu.mx>
2024-03-15 09:58:25 -0600

    1er Commit

---
+++ src/mx/cesba/edu/HolaMundo.java
@@ -0,0 +1,31 @@
+package mx.cesba.edu;
+
+
+public class HolaMundo {
+    public static void main(String[]args){
+       /*
+       int edad = 17;
+       int mayorEdad;
+       
+       mayorEdad = edad+1;
+       
+        
+        System.out.println("bienvenidos al mundo de la programacoion");
+        System.out.println("El joven es menor de edad:"+ edad);
+        System.out.println("Ya es un adulto "+ mayorEdad);
+       
+       float precio = 78.93F;
+       float iva = 10.01f;
+       //float preciof;
+       float total = precio+iva;
+       System.out.println ("El precio total es:"precio+iva);
+       Char corchete = 93;
+       Sistem.out.println(Corchete)
+        
+       long suma1= 987787767;
+       long suma2= 877766666;
+        */
+    }
+    
+    
+}
